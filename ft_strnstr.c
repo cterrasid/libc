ff767bc13b70c0300467194555fa3081056cb26e
Clarette Terrasi diaz <cterrasi@c2r16s6.42madrid.com>
2022-03-27 17:03:54 +0200

    test

---
+++ ft_strnstr.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	if (*little == '\0')
+		return (*big);
+	while (len--)
+	{
+	}
+	return (NULL);
+}
+
+int	main(void)
+{
+	return (0);
+}
