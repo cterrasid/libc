fbbff74a212c46ffd0851433f75dbc60347ab97a
Clarette Terrasi Diaz <clarette.terrasi@gmail.com>
2022-03-27 12:06:06 +0200

    Add an advance of ft_strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+char *ft_strnstr(const char *big, const char *little, size_t len)
+{
+    if (*little == '\0')
+        return (*big);
+    while (len--)
+    {
+    }
+    return (NULL);
+}
+
+int main(void)
+{
+
+    return (0);
+}
