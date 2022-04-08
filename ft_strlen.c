54caf095f65c413516a4da9d6ef742ca4ad1b845
clarettetd <clarette.terrasi@gmail.com>
2022-04-08 18:56:03 +0200

    clean(ft_strlen): remove the main and unecessary comments

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,31 +6,18 @@
 /*   By: cterrasi <cterrasi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 16:56:48 by cterrasi          #+#    #+#             */
-/*   Updated: 2022/04/02 17:59:40 by cterrasi         ###   ########.fr       */
+/*   Updated: 2022/04/08 18:07:06 by cterrasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/* -------------------------------------------------------------------------- */
-/* Calcuates the length of a given string 's'.								  */
-/* -------------------------------------------------------------------------- */
 
 #include "libft.h"
 
 size_t	ft_strlen(const char *s)
 {
-	size_t	i;
+	const char	*s2;
 
-	i = 0;
-	while (*s++)
-		i++;
-	return (i);
+	s2 = s;
+	while (*s)
+		s++;
+	return (s - s2);
 }
-
-// int	main(void)
-// {
-// 	char	s[] = "Hola";
-// 	printf("Original: %lu\n", strlen(s));
-// 	printf("Mine: %zu\n", ft_strlen(s));
-	// system("leaks a.out");
-// 	return (0);
-// }
