bf98fc708ab70eb73af9f1ed62a2bfd506c61a8a
clarettetd <clarette.terrasi@gmail.com>
2022-04-08 18:43:39 +0200

    clean(ft_toupper): remove the main and unnecessary comments

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,13 +6,9 @@
 /*   By: cterrasi <cterrasi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 13:36:01 by cterrasi          #+#    #+#             */
-/*   Updated: 2022/04/02 18:14:52 by cterrasi         ###   ########.fr       */
+/*   Updated: 2022/04/08 18:18:27 by cterrasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/* -------------------------------------------------------------------------- */
-/* Takes a lowercase alphabet and convert it to a uppercase character.		  */
-/* -------------------------------------------------------------------------- */
 
 #include "libft.h"
 
@@ -22,12 +18,3 @@ int	ft_toupper(int c)
 		c -= 32;
 	return (c);
 }
-
-// int	main(void)
-// {
-// 	int	c = 'a';
-// 	printf("Mine: %i\n", ft_toupper(c));
-// 	printf("Original: %i\n", toupper(c));
-	// system("leaks a.out");
-// 	return (0);
-// }
