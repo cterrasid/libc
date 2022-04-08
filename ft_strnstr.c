6f4c55caf4df5de014ba149806dbe33aa222fbab
clarettetd <clarette.terrasi@gmail.com>
2022-04-08 18:53:33 +0200

    clean(ft_strnstr): remove the main and unecessary comments

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,14 +6,9 @@
 /*   By: cterrasi <cterrasi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 17:05:08 by cterrasi          #+#    #+#             */
-/*   Updated: 2022/04/02 18:05:47 by cterrasi         ###   ########.fr       */
+/*   Updated: 2022/04/08 18:11:32 by cterrasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/* -------------------------------------------------------------------------- */
-/* Locates the first occurrence of 'needle' in the first 'len' bytes of		  */
-/* 'haystack'. 																  */
-/* -------------------------------------------------------------------------- */
 
 #include "libft.h"
 
@@ -33,16 +28,3 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	}
 	return (NULL);
 }
-
-// int	main(void)
-// {
-// 	const char haystack[] = "aaabcabcd";
-// 	const char needle[] = "cd";
-// 	size_t len = 8;
-// 	printf("strnstr: %s\n", ft_strnstr(haystack, needle, len));
-// 	printf("strnstr: %d\n", strnstr(haystack, needle, len));
-// 	printf("memcmp: %d\n", ft_memcmp(haystack, needle, 2));
-// 	printf("strncmp: %d\n", ft_strncmp(haystack, needle, 2));
-	// system("leaks a.out");
-// 	return (0);
-// }
