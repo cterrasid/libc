ce8fd46cc48d47fb076d22e014e41b163f96634d
clarettetd <clarette.terrasi@gmail.com>
2022-04-08 18:44:25 +0200

    clean(ft_tolower): remove the main and unnecessary comments

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,13 +6,9 @@
 /*   By: cterrasi <cterrasi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 16:43:08 by cterrasi          #+#    #+#             */
-/*   Updated: 2022/04/02 18:14:24 by cterrasi         ###   ########.fr       */
+/*   Updated: 2022/04/08 18:18:17 by cterrasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/* -------------------------------------------------------------------------- */
-/* Takes an uppercase alphabet and convert it to a lowercase character.		  */
-/* -------------------------------------------------------------------------- */
 
 #include "libft.h"
 
@@ -22,12 +18,3 @@ int	ft_tolower(int c)
 		c += 32;
 	return (c);
 }
-
-// int	main(void)
-// {
-// 	int	c = 'a';
-// 	printf("Mine: %i\n", ft_tolower(c));
-// 	printf("Original: %i\n", tolower(c));
-	// system("leaks a.out");
-// 	return (0);
-// }
