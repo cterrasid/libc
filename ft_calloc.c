b244bec5b37f9431dabded8f4ff88bcf58730e09
clarettetd <clarette.terrasi@gmail.com>
2022-04-15 21:31:27 +0200

    feat(ft_calloc.c): update function

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: cterrasi <cterrasi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 21:22:56 by cterrasi          #+#    #+#             */
-/*   Updated: 2022/04/02 17:20:34 by cterrasi         ###   ########.fr       */
+/*   Updated: 2022/04/08 15:14:00 by cterrasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	unsigned char	*mem;
+	unsigned char	*p;
 	size_t			i;
 
-	mem = malloc(count * size);
-	if (mem != NULL)
+	p = malloc(count * size);
+	if (p && (size <= UINT_MAX || count <= UINT_MAX))
 	{
 		i = 0;
 		while (i < count * size)
-			*(mem + i++) = '\0';
+			*(p + i++) = '\0';
+		return (p);
 	}
-	return (mem);
+	return (NULL);
 }
