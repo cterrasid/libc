77dff7b152c2230c4171171e9d5830622003d4a5
clarettetd <clarette.terrasi@gmail.com>
2022-04-15 21:36:02 +0200

    feat(ft_memmove.c): update the function

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: cterrasi <cterrasi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 20:42:27 by cterrasi          #+#    #+#             */
-/*   Updated: 2022/04/02 17:35:50 by cterrasi         ###   ########.fr       */
+/*   Updated: 2022/04/08 18:21:48 by cterrasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,10 @@ void	*ft_memmove(void *dest, const void *src, size_t len)
 	s = (char *)src;
 	if (s < dst)
 	{
-		dst += len - 1;
-		s += len - 1;
 		while (len--)
-			*dst-- = *s--;
+				*(dst + len) = *(s + len);
+		return (dest);
 	}
 	else
-		ft_memcpy(dest, src, len);
-	return (dest);
+		return (ft_memcpy(dest, src, len));
 }
