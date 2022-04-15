16ab8c03446c692819e2390c14384719e26fadb1
clarettetd <clarette.terrasi@gmail.com>
2022-04-15 21:39:52 +0200

    feat(ft_strlcat.c): update the function

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,27 +6,27 @@
 /*   By: cterrasi <cterrasi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 17:58:20 by cterrasi          #+#    #+#             */
-/*   Updated: 2022/04/02 17:56:42 by cterrasi         ###   ########.fr       */
+/*   Updated: 2022/04/13 00:11:35 by cterrasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
+	size_t	i;
+	size_t	j;
 	size_t	slen;
 	size_t	dlen;
 
 	dlen = ft_strlen((const char *)dst);
 	slen = ft_strlen(src);
-	if (dstsize <= dlen)
-		return (slen + dstsize);
-	dst += dlen;
-	while (*src && dstsize)
-	{
-		*dst++ = *src++;
-		dstsize--;
-	}
-	dst[dstsize] = '\0';
+	i = 0;
+	j = dlen;
+	if (size <= dlen)
+		return (slen + size);
+	while (src[i] && i < size - dlen - 1)
+		dst[j++] = src[i++];
+	dst[j] = '\0';
 	return (dlen + slen);
 }
