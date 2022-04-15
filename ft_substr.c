79b0156a88f163c08549a6f213c945a1232f5c80
clarettetd <clarette.terrasi@gmail.com>
2022-04-15 21:43:21 +0200

    feat(ft_substring.c): update the function

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: cterrasi <cterrasi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 04:04:09 by cterrasi          #+#    #+#             */
-/*   Updated: 2022/04/08 18:16:19 by cterrasi         ###   ########.fr       */
+/*   Updated: 2022/04/13 01:24:45 by cterrasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,24 @@
 char	*ft_substr(const char *str, unsigned int start, size_t max_len)
 {
 	unsigned int	i;
-	size_t			len;
+	size_t			slen;
 	size_t			substr_len;
 	char			*substring;
 
-	if (str)
-	{
-		i = 0;
-		len = ft_strlen(str);
-		substr_len = ft_strlen(str + start);
-		substring = (char *)malloc(sizeof(char) * substr_len + 1);
-		if (!substring)
-			return (NULL);
-		if (start > len)
-			max_len = 0;
-		if (substr_len < max_len)
-			max_len = substr_len;
-		while (i < max_len)
-			substring[i++] = str[start++];
-		substring[i] = '\0';
-		return (substring);
-	}
-	return (NULL);
+	if (!str)
+		return (NULL);
+	i = 0;
+	slen = ft_strlen(str);
+	substr_len = ft_strlen(str + start);
+	if (slen < start)
+		max_len = 0;
+	if (substr_len < max_len)
+		max_len = substr_len;
+	substring = (char *)malloc(sizeof(char) * max_len + 1);
+	if (!substring)
+		return (NULL);
+	while (max_len--)
+		substring[i++] = str[start++];
+	substring[i] = '\0';
+	return (substring);
 }
