45ab4a2b344983510b005eefa750b0d5a48c0e08
clarettetd <clarette.terrasi@gmail.com>
2022-04-15 21:38:44 +0200

    feat(ft_strdup.c): update the function

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: cterrasi <cterrasi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 20:31:36 by cterrasi          #+#    #+#             */
-/*   Updated: 2022/04/02 17:45:40 by cterrasi         ###   ########.fr       */
+/*   Updated: 2022/04/08 18:00:11 by cterrasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 char	*ft_strdup(const char *s)
 {
 	char	*d;
-	size_t	len;
+	size_t	s_len;
 
-	len = ft_strlen(s);
-	d = (char *)malloc(sizeof(char) * len + 1);
+	s_len = ft_strlen(s);
+	d = (char *)malloc(sizeof(char) * s_len + 1);
 	if (d == NULL)
 		return (NULL);
-	ft_memcpy(d, s, len);
-	d[len] = '\0';
+	ft_memcpy(d, s, s_len);
+	d[s_len] = '\0';
 	return (d);
 }
